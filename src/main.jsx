782d26857e3c89b58f378fd110161f3fcc3d5564
manoel <manoelsalgadoneto@gmail.com>
2023-04-27 21:43:46 -0300

    1a aula do Stage 09

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Details } from './pages/Details';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/global';
+
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>
 )
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,17 @@
+import { Container } from './styles';
+
+import { Button } from '../../components/Button';
+
+export function Details(){
+
+  return(
+    <Container>
+      <h1>Hello World!</h1>
+      <span>Manoel Salgado</span>
+
+      <Button title="Login" loading />
+      <Button title="Cadastrar"/>
+      <Button title="Voltar"/>
+    </Container>
+   )
+}
