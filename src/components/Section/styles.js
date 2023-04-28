4ca6257fa6acc0a50741f721a1007931b2df30c4
manoel <manoelsalgadoneto@gmail.com>
2023-04-28 19:07:32 -0300

    Aula do Stage 09

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  margin: 56px 0 28px;
+  margin: 28px 0;
 
   > h2 {
     border-bottom-width: 1px;
@@ -9,9 +9,9 @@ export const Container = styled.section`
     border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUD_700};
 
     padding-bottom: 16px;
-    margin-bottom: 24px;
+    margin-bottom: 28px;
 
-    color: ${({theme }) => theme.COLORS.GRAY};
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
     font-size: 20px;
     font-weight: 400;
    }
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Details } from './pages/Details';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 
 import theme from './styles/theme';
 
+import { Home } from './pages/Home';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 )
