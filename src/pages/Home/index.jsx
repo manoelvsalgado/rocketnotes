28fb160414819aaa39cd5e42bfdacd14ddb0f6fb
manoel <manoelsalgadoneto@gmail.com>
2023-04-28 21:46:57 -0300

    criação de notas e tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,10 @@
+import { FiPlus} from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 import { Header } from '../../components/Header';
+import { ButtonText } from '../../components/ButtonText';
+import { Note } from '../../components/note';
+import { Input } from '../../components/Input';
+import { Section } from '../../components/Section';
 
 export function Home() {
   return (
@@ -12,19 +17,32 @@ export function Home() {
       <Header />
 
       <Menu>
+        <li><ButtonText title="Todos" /></li>
+        <li><ButtonText title="React" /></li>
+        <li><ButtonText title="Node" /></li>     
 
       </Menu>
 
       <Search>
-
+        <Input placeholder="Pesquisar pelo título"/>
       </Search>
 
       <Content>
-
+        <Section title="Minhas notas"> 
+          <Note data={{
+            title: 'React', 
+            tags: [
+             {id:'1', name: 'react'},
+             {id:'2', name: 'rocketseat'}
+            ]
+           }}
+           />
+        </Section>
       </Content>
 
       <NewNote>
-
+        <FiPlus />
+        Criar Nota
       </NewNote>
 
     </Container>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -13,31 +13,63 @@ export const Container = styled.div`
   "menu content"
   "newnote content";
 
-  background-color: ${({ theme }) => theme.COLOR.BACKGROUND_800};
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
 `;
 
 export const Brand = styled.div`
    grid-area: brand;
-   background: red;
+   
+   display: flex;
+   justify-content: center;
+   align-items: center;
+
+   border-bottom-width: 1px;
+   border-bottom-style: solid;
+   border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+   > h1 {
+    font-size: 24px;
+    color: ${({ theme }) => theme.COLORS.ORANGE}
+   }
 `;
 
 export const Menu = styled.ul`
    grid-area: menu;
-   background: green;
+   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+   padding-top: 64px;
+   text-align: center;
+   
+   > li {
+      margin-bottom: 24px;
+   } 
 `;
   
 
 export const Search = styled.div`
    grid-area: search;
-   background: violet;
+   padding: 64px 64px 0;
+   
 `;
 
 export const Content = styled.div`
    grid-area: content;
-   background: blue;
+   padding: 0 64px;
+   overflow-y: auto;
 `;
 
 export const NewNote= styled.button`
    grid-area: newnote;
-   background: yellow;
+   background-color: ${({ theme }) => theme.COLORS.ORANGE};
+   border: none;
+
+   display: flex;
+   align-items: center;
+   justify-content: center;
+
+   svg {
+   margin-right: 8px;
+   }
+
 `;
